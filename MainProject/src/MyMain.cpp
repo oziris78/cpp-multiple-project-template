6bee2b065164893978ffc178413e65bb272a35d0
OziRis78 <oguzhantg3@gmail.com>
2022-02-27 23:52:50 +0300

    🦮🦮 added imgui

---
+++ MainProject/src/MyMain.cpp
@@ -0,0 +1,19 @@
+
+#include "Functions.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+// Uncomment these to see it work, each project has to have one main function
+// that's why this main function commented out.
+
+/*
+int main() {
+	// MY OWN STATIC LIBRARY
+	telek::print_hi();
+
+	// ADDING GLFW AS A STATIC LIBRARY
+	int a = glfwInit();
+	std::cout << a << std::endl;
+}
+*/
